8293adfa34b57275a487a587d29287b550d6e465
fwastring <fredrik@wastring.com>
2025-04-07 16:36:46 +0200

    Changed frontend and backend

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Backend
+
+`make dev`
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <div class="bg-red-300">
-        HEJHEJHEJ
+        Hej Jonathan
       </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
